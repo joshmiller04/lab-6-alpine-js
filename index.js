bb73cea6ee47051b131ab1d726f3409c5bda70b7
joshmiller04 <mill3643@msu.edu>
2025-02-19 17:07:41 +0000

    alpine js

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// Add JavaScript below
+function randomNameGenerator() {
+    return {
+        name: "Click the button to get a name",
+        names: [
+            "Josh", "Jimmy", "Tom", "David", "Bob",
+            "Frank", "Tim", "sara", "Lindsay", "Jack"
+        ],
+        generateRandomName() {
+            this.name = this.names[Math.floor(Math.random() * this.names.length)];
+        }
+    };
+}
